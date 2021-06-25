75d1aa418f278f7cd4f8d8f43f0cd3b349f08870
jamk2k <chacha22@mail.com>
2021-06-25 18:41:18 +0900

    Update app.js

--- CSM-demo/app.js
+++ CSM-demo/app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 松山webhookテスト')
+  res.send('Hello World! 松山webhookテスト 0625')
 })
 
 app.listen(port, () => {
